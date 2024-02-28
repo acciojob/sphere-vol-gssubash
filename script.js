f7c863b31504ca09061bac6b41b572a3334bb0a9
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-28 09:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	const form = document.getElementById('MyForm');
+	
+	const rad = Number(MyForm.radius.value);
+	const ans = (Math.PI*4*(rad**3))/3;
+	form.volume.value = ans;
+  return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
